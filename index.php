b4ab14840107adafdfff3b6cc57785b90aa0eb31
unknown <nawalnawal8@gmail.com>
2022-07-17 21:58:14 -0400

    add better user input for dropping a beekin

--- index.php
+++ index.php
@@ -15,32 +15,27 @@
 <link href="toggle_styles.css" rel="stylesheet">
 <link href="slider_style.css" rel="stylesheet">
 <link href="logo_container_style.css" rel="stylesheet">
-<script src="mapbox_scripts.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
 
 <link rel="icon" href="icon.png">
 
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<script src="mapbox_scripts.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<!--<script src="jquery.cookie.js"></script> https://github.com/carhartl/jquery-cookie -->
-<script src="jquery.cookie.js"></script>
+<script src="jquery.cookie.js"></script> <!--https://github.com/carhartl/jquery-cookie-->
 <script>
+	/* stores cookie. if cookie is stored, do not display modal. show modal if no cookie. SO: https://stackoverflow.com/questions/13352658/reveal-modal-with-cookie-to-display-only-once */
 
-	
-	/*<!-- stores cookie. if cookie is stored, do not display modal. show modal if no cookie. SO: https://stackoverflow.com/questions/13352658/reveal-modal-with-cookie-to-display-only-once -->*/
 	/*$(document).ready(function() {
     if ($.cookie('beekins_modal_shown') == null) {
         $.cookie('beekins_modal_shown', 'yes', { expires: 1, path: '/' });
         $("#intro_modal").modal('show');
     }
 });*/
-	
-	
 </script>
 
 <style>
-
 </style>
 
 </head>
@@ -56,6 +51,15 @@
 
 <pre id="coordinates" class="coordinates"></pre>
 
+<div class="map-overlay top">
+	<div class="map-overlay-inner" style="filter:alpha(opacity=50); opacity:0.75;">
+		<h3>Show Beekins &#128029; Near You</h3>
+		<p>Move the slider to show events near you.</p>
+		<p>Showing beekins in a <span id="demo"></span> mile radius</p>
+		<!--<label id="month"></label>-->
+		<input id="myRange" type="range" min="1" max="100" step="1" value="10" onchange="location.reload()">
+	</div>
+</div>
 
 <div id="intro_modal" class="modal fade">
     <div class="modal-dialog">
@@ -86,7 +90,25 @@
             	<p>Longitude: <span id="beekin-longitude"></span></p>
             	<p>Latitude: <span id="beekin-latitude"></span></p>
 				<!--<button type="submit" class="btn btn-primary" data-dismiss="modal">Start Navigation</button>-->
-				<button type="submit" class="btn btn-primary" data-dismiss="modal" onclick="startNav()">Start Navigation</button>
+				<button type="button" class="btn btn-primary" data-dismiss="modal" onclick="startNav()">Start Navigation</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="drop_beekin_modal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Dropping a Beekin &#128029;</h5>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+            	<label for="beekin-description">Beekin description:</label>
+            	<input type="text" id="beekin-description" name="beekin-description" class="form-control input-lg" placeholder="Beekins Launch Party!">
+            	<small id="small-beekin-description" class="form-text text-muted">Do <strong>not</strong> include personal information in description.</small>
+            	<p style="padding:1px"></p>
+				<button id="drop-beekin-description" type="button" class="btn btn-primary" data-dismiss="modal">Drop Beekin</button>
             </div>
         </div>
     </div>
@@ -103,16 +125,6 @@
 	
 	<input id="dark-v10" type="radio" name="rtoggle" value="dark">
 	<label for="dark-v10">dark</label>	
-</div>
-
-<div class="map-overlay top">
-	<div class="map-overlay-inner" style="filter:alpha(opacity=50); opacity:0.75;">
-		<h3>Show Beekins &#128029; Near You</h3>
-		<p>Move the slider to show events near you.</p>
-		<p>Showing beekins in a <span id="demo"></span> mile radius</p>
-		<!--<label id="month"></label>-->
-		<input id="myRange" type="range" min="1" max="100" step="1" value="10" onchange="location.reload()">
-	</div>
 </div>
  
 </body>
